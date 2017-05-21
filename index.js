78b3d90ff92b9535fa1b05dfc7edbd391383eebe
leoyuholo <leoyuholo@gmail.com>
2017-05-21 13:32:37 +0800

    add coverage

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./lib/app');
 
-app.listen(3000, () => {
-	console.log('server is listening on port 3000');
+app.listen(3030, () => {
+	console.log('server is listening on port 3030');
 });
